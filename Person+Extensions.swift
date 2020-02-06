81ab9137db3695280127672f2f1fe8e0a14624be
Kevin Yien <yien.kevin@gmail.com>
2020-02-05 22:38:06 -0500

    adding all project files

---
+++ Person+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Person+Extensions.swift
+//  wei
+//
+//  Created by Kevin Yien on 2/5/20.
+//  Copyright © 2020 Kevin Yien. All rights reserved.
+//
+
+import Foundation
+
+extension Person: Identifiable {
+    
+}
